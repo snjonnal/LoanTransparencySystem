62268fa4d5f313037475650c68e55ff910039089
snjonnal <snjonnal@in.ibm.com>
2017-05-07 19:11:23 +0530

    first commit

---
+++ routes/site_router.js
@@ -0,0 +1,49 @@
+'use strict';
+/* global process */
+/*******************************************************************************
+ * Copyright (c) 2015 IBM Corp.
+ *
+ * All rights reserved.
+ *******************************************************************************/
+var express = require('express');
+var router = express.Router();
+var setup = require('../setup.js');
+
+//anything in here gets passed to JADE template engine
+function build_bag(){
+	return {
+				setup: setup,								//static vars for configuration settings
+				e: process.error,							//send any setup errors
+				jshash: process.env.cachebust_js,			//js cache busting hash (not important)
+				csshash: process.env.cachebust_css,			//css cache busting hash (not important)
+			};
+}
+
+// ============================================================================================================================
+// Home
+// ============================================================================================================================
+router.route('/').get(function(req, res){
+	res.redirect('/p1');
+});
+
+// ============================================================================================================================
+// Part 1
+// ============================================================================================================================
+router.route('/p1').get(function(req, res){
+	res.render('part1', {title: 'LTS', bag: build_bag()});
+});
+router.route('/p1/:page?').get(function(req, res){
+	res.render('part1', {title: 'LTS', bag: build_bag()});
+});
+
+// ============================================================================================================================
+// Part 2
+// ============================================================================================================================
+router.route('/p2').get(function(req, res){
+	res.render('part2', {title: 'LTS', bag: build_bag()});
+});
+router.route('/p2/:page?').get(function(req, res){
+	res.render('part2', {title: 'LTS', bag: build_bag()});
+});
+
+module.exports = router;
---
+++ setup.js
@@ -0,0 +1,67 @@
+/* global process */
+/*******************************************************************************
+ * Copyright (c) 2015 IBM Corp.
+ *
+ * All rights reserved.
+ *
+ * 
+ *******************************************************************************/
+//Environments are either:
+// 	1 - Bluemix Production
+// 	2 - Bluemix Development
+// 	3 - Localhost Development
+
+var vcap_app = {application_uris: ['']};						//default blank
+var ext_uri = '';
+if(process.env.VCAP_APPLICATION){
+	vcap_app = JSON.parse(process.env.VCAP_APPLICATION);
+	for(var i in vcap_app.application_uris){
+		if(vcap_app.application_uris[i].indexOf(vcap_app.name) >= 0){
+			ext_uri = vcap_app.application_uris[i];
+		}
+	}
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////    1. Bluemix Production    ////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+if(process.env.PORT && process.env.PRODUCTION){
+	exports.SERVER = 	{
+							HOST: '0.0.0.0',
+							PORT: process.env.PORT,
+							DESCRIPTION: 'Bluemix - Production',
+							EXTURI: ext_uri,		//no longer used 4/29/2016
+						};
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////    2. Bluemix Development    ///////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+else if(process.env.PORT){
+		exports.SERVER = 	{
+								HOST: '0.0.0.0',
+								PORT: process.env.PORT,
+								DESCRIPTION: 'Bluemix - Development',
+								EXTURI: ext_uri,		//no longer used 4/29/2016
+							 };
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////     3. Localhost - Development    ////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+else{
+	exports.SERVER = 	{
+							HOST:'localhost',
+							PORT: 3000,
+							DESCRIPTION: 'Localhost',
+							EXTURI: 'localhost:3000',	//no longer used 4/29/2016
+						 };
+}
+
+exports.SERVER.vcap_app = vcap_app;
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////     Common     ////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+exports.DEBUG = vcap_app;
+exports.USER1 = 'bob';									//left username
+exports.USER2 = 'leroy';								//right username
